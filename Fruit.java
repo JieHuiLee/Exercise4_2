087187e064ccc9473a0bfdf2dc1b7bf02b43a83f
JieHuiLee <81312864+JieHuiLee@users.noreply.github.com>
2021-04-29 08:54:08 +0800

    Create Fruit.java

---
+++ Fruit.java
@@ -0,0 +1,23 @@
+package Exercise4_2;
+
+public class Fruit { //superclass
+	protected String name;
+	
+	public Fruit(){ //overloading without argument
+		this.name = " ";
+	}
+	
+	public Fruit(String name){ //overloading with 1 argument
+		this.name = name;
+		System.out.println(name+ " constructor is invoked.");
+	}
+	
+	public String getName() {
+		return name;
+	}		
+	
+	public String toString() { //overriding method
+		return "Eat more "+ name + " will be more healthy.";
+	}		
+
+}// end for Fruit class
